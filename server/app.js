c194aef799e7547526d858d4b0016f351782aa90
Nishi Kantamneni <nk475@cornell.edu>
2022-01-09 12:51:25 -0800

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+const cors = require('cors');
+
+
+const app = express();
+
+//allow cross-origin requests
+app.use(cors());
+
+//connec to mlab database
+mongoose.connect('mongodb+srv://admin:admin@cluster0.0bqtz.mongodb.net/graphql-books?retryWrites=true&w=majority')
+mongoose.connection.once('open',()=>{
+  console.log('connected to database');
+})
+
+app.use("/graphql", graphqlHTTP({
+  schema,
+  graphiql:true
+}));
+
+
+app.listen(4000,() => {
+  console.log('now listening for requests on port 4000')
+});
